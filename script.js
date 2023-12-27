51e3bbd41b32a7d3673f0bb3c2db2e3b04f076cd
TobiTheDeveloper <yusuffoyediranyo@gmail.com>
2023-12-26 20:49:06 -0500

    Adding html

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
+const { query } = require("express");
+
 //API Key
 const API_KEY = "da4b66d16efcf73caaa311e01941af0b";
+const baseURL = "https://image.tmdb.org/t/p/w500";
+const posterPath = "/example_poster.jpg";
+const fullPosterURL = baseURL + posterPath;
+// Result: "https://image.tmdb.org/t/p/w500/example_poster.jpg"
+
+// APIs
+const apiPaths = {
+    searchMovie: (query) => ''
+}
