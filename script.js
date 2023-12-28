a2633d3ab6cd403e4b4d8d224508e9bb01cb9705
TobiTheDeveloper <yusuffoyediranyo@gmail.com>
2023-12-28 01:11:18 -0500

    Now adding Functions

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const { query } = require("express");
-
 //API Key
 const API_KEY = "da4b66d16efcf73caaa311e01941af0b";
 const baseURL = "https://image.tmdb.org/t/p/w500";
@@ -9,5 +7,29 @@ const fullPosterURL = baseURL + posterPath;
 
 // APIs
 const apiPaths = {
-    searchMovie: (query) => ''
+    searchMovie: (query) => `https://api.themoviedb.org/3/search/movie?query=${query}&api_key=${API_KEY}`,
+    findGenres: `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`,
+    findCast: (movie_id) => `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${API_KEY}`,
+    findPerson: (person_id) => `https://api.themoviedb.org/3/person/${person_id}?api_key=${API_KEY}`,
 }
+
+const searchInput = document.querySelector('.main__searchField input');
+const searchMovieListContainer = document.querySelector('.movie-list-container');
+const movieContainer = document.querySelector('.movie-container');
+const castContainer = document.querySelector('.movie-container .cast-details-container');
+const castDetailsContainer = document.querySelector('.movie-container .cast-details-container .cast-details');
+
+
+const searchMovie = (e) => {
+    const res = fetch(apiPaths.searchMovie(searchInput.value));
+
+    res
+    .then(res => res.json())
+    .then(res => {
+        buildSearchMovieList(res.results.slice(0,10));
+    })
+    .catch(error => {
+        console.log(error);
+    });
+};
+
